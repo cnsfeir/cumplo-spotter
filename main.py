69b5c7912a37131ce056a3bd354519d44811aebd
Cristobal Sfeir <cnsfeir@uc.cl>
2023-03-26 20:00:46 -0300

    🔊 Add GCP logging integration

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ from logging import CRITICAL, DEBUG, basicConfig, getLogger
 
 import functions_framework
 from flask import Request, Response, make_response
+from google.cloud import logging
 
 from integrations.cumplo import get_funding_requests
 from middlewares.authentication import authenticate
 from models.user import User
 from utils.event import get_configuration
+
+client = logging.Client()
+client.setup_logging()
+
 
 IS_TESTING = bool(os.getenv("IS_TESTING"))
 
