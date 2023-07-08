da62397d7654c1f0f7fc5f8644825fe1ffe167ed
Cristobal Sfeir <cnsfeir@uc.cl>
2023-07-08 19:10:49 -0400

    ⚰️ Remove AvailableFilter
    Since unavailable funding requests are filtered out by default

--- models/filter.py
+++ models/filter.py
@@ -21,23 +21,6 @@ class Filter(metaclass=ABCMeta):
         """
 
 
-class AvailableFilter(Filter):
-    def __init__(self, user: User) -> None:
-        self.user = user
-
-    def apply(self, funding_request: FundingRequest) -> bool:
-        """
-        Filters out the funding requests that are already completed.
-        """
-        if not funding_request.is_completed:
-            return True
-
-        if self.user.notifications.get(funding_request.id):
-            firestore_client.delete_notification(self.user.id, funding_request.id)
-
-        return False
-
-
 class NotificationFilter(Filter):
     def __init__(self, configuration: Configuration, user: User) -> None:
         self.configuration = configuration
