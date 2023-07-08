153da4c456f111591b9e4796393c592d0b32f849
Cristobal Sfeir <cnsfeir@uc.cl>
2023-07-08 19:35:20 -0400

    🚧 Add /fetch internal endpoint

--- routers/funding_requests.py
+++ routers/funding_requests.py
@@ -6,8 +6,10 @@ from fastapi import APIRouter
 from fastapi.requests import Request
 
 from integrations import cumplo
+from integrations.firestore import firestore_client
 from models.funding_request import FundingRequest
 from models.user import User
+from schemas.funding_requests import FilterFundingRequestPayload
 
 logger = getLogger(__name__)
 
@@ -39,15 +41,27 @@ async def get_promising_funding_requests(request: Request) -> list[FundingReques
     return list(promising_funding_requests)
 
 
+@internal.post(path="/fetch", status_code=HTTPStatus.OK)
+async def fetch_funding_requests(_request: Request) -> None:
+    """
+    Fetches a list of funding requests and schedules the filtering process.
+    """
+    funding_requests = cumplo.get_available_funding_requests()
+
+    for user in firestore_client.get_users():
+        # TODO: Schedule a taks to filter the funding requests and send webhooks
+        pass
+
+
 @internal.post(path="/filter", status_code=HTTPStatus.OK)
-async def filter_funding_requests(request: Request, funding_requests: list[FundingRequest]) -> list[FundingRequest]:
+async def filter_funding_requests(_request: Request, payload: FilterFundingRequestPayload) -> None:
     """
     Filters a list of funding requests based on the user's configuration.
     """
-    user = cast(User, request.state.user)
+    user = firestore_client.get_user(payload.id_user)
 
     promising_funding_requests = set()
     for configuration in user.configurations.values():
-        promising_funding_requests.update(cumplo.filter_funding_requests(funding_requests, user, configuration))
+        promising_funding_requests.update(cumplo.filter_funding_requests(payload.funding_requests, user, configuration))
 
-    return list(promising_funding_requests)
+    # TODO: Send webhooks
