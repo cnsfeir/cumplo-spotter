3edecf9354e41a066fd7e0b2cd76b5de3e9d3f66
Cristobal Sfeir <cnsfeir@uc.cl>
2023-07-08 14:44:45 -0400

    ♻️ Return a model directly

--- routers/funding_requests.py
+++ routers/funding_requests.py
@@ -1,30 +1,31 @@
+from http import HTTPStatus
 from logging import getLogger
 
 from fastapi import APIRouter
 from fastapi.requests import Request
-from fastapi.responses import JSONResponse
 
 from integrations.cumplo import get_available_funding_requests
+from models.funding_request import FundingRequest
 
 logger = getLogger(__name__)
 
 router = APIRouter(prefix="/funding-requests")
 
 
-@router.get("")
-async def get_funding_requests(_request: Request) -> JSONResponse:
+@router.get("", status_code=HTTPStatus.OK)
+async def get_funding_requests(_request: Request) -> list[FundingRequest]:
     """
     Gets a list of available funding requests.
     """
     funding_requests = get_available_funding_requests()
-    return JSONResponse(status_code=200, content=funding_requests)
+    return funding_requests
 
 
-@router.get("/promising")
-async def get_promising_funding_requests(_request: Request) -> JSONResponse:
+@router.get("/promising", status_code=HTTPStatus.OK)
+async def get_promising_funding_requests(_request: Request) -> list[FundingRequest]:
     """
     Gets a list of promising funding requests based on the user's configuration.
     """
     funding_requests = get_available_funding_requests()
     # TODO: Filter funding requests based on ALL of user's configurations
-    return JSONResponse(status_code=200, content=funding_requests)
+    return funding_requests
