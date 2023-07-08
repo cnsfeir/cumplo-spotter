ac7b7dab720fb6951a487a8ca0b92441a9cc8ecc
Cristobal Sfeir <cnsfeir@uc.cl>
2023-07-08 19:12:35 -0400

    ✨ Finish funding-requests router

--- routers/funding_requests.py
+++ routers/funding_requests.py
@@ -1,15 +1,18 @@
 from http import HTTPStatus
 from logging import getLogger
+from typing import cast
 
 from fastapi import APIRouter
 from fastapi.requests import Request
 
-from integrations.cumplo import get_available_funding_requests
+from integrations import cumplo
 from models.funding_request import FundingRequest
+from models.user import User
 
 logger = getLogger(__name__)
 
 router = APIRouter(prefix="/funding-requests")
+internal = APIRouter(prefix="/funding-requests")
 
 
 @router.get("", status_code=HTTPStatus.OK)
@@ -17,15 +20,34 @@ async def get_funding_requests(_request: Request) -> list[FundingRequest]:
     """
     Gets a list of available funding requests.
     """
-    funding_requests = get_available_funding_requests()
+    funding_requests = cumplo.get_available_funding_requests()
     return funding_requests
 
 
 @router.get("/promising", status_code=HTTPStatus.OK)
-async def get_promising_funding_requests(_request: Request) -> list[FundingRequest]:
+async def get_promising_funding_requests(request: Request) -> list[FundingRequest]:
     """
     Gets a list of promising funding requests based on the user's configuration.
     """
-    funding_requests = get_available_funding_requests()
-    # TODO: Filter funding requests based on ALL of user's configurations
-    return funding_requests
+    funding_requests = cumplo.get_available_funding_requests()
+    user = cast(User, request.state.user)
+
+    promising_funding_requests = set()
+    for configuration in user.configurations.values():
+        promising_funding_requests.update(cumplo.filter_funding_requests(funding_requests, user, configuration))
+
+    return list(promising_funding_requests)
+
+
+@internal.post(path="/filter", status_code=HTTPStatus.OK)
+async def filter_funding_requests(request: Request, funding_requests: list[FundingRequest]) -> list[FundingRequest]:
+    """
+    Filters a list of funding requests based on the user's configuration.
+    """
+    user = cast(User, request.state.user)
+
+    promising_funding_requests = set()
+    for configuration in user.configurations.values():
+        promising_funding_requests.update(cumplo.filter_funding_requests(funding_requests, user, configuration))
+
+    return list(promising_funding_requests)
