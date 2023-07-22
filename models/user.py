36367f2d2f8cce8eedb5583e9d10e78aa070bdb0
Cristobal Sfeir <cnsfeir@uc.cl>
2023-07-22 18:24:18 -0400

    ⚰️ Remove unused functions from User model

--- models/user.py
+++ models/user.py
@@ -1,25 +1,15 @@
 # pylint: disable=no-member
 
-from datetime import datetime
-
-import arrow
 from pydantic import BaseModel, Field
 
 from models.configuration import Configuration
 from models.notification import Notification
-from utils.constants import SANTIAGO_TIMEZONE
 
 
 class User(BaseModel):
     id: str = Field(...)
     name: str = Field(...)
     calls: int = Field(...)
-    last_call: datetime = Field(...)
     webhook_url: str | None = Field(None)
     notifications: dict[int, Notification] = Field(default_factory=dict, exclude=True)
     configurations: dict[int, Configuration] = Field(default_factory=dict, exclude=True)
-
-    def register_call(self) -> None:
-        """Registers the last call date for a user"""
-        self.last_call = arrow.now(SANTIAGO_TIMEZONE).datetime
-        self.calls += 1
