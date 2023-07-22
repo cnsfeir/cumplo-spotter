530621b03a957db24a78cbe9e68e4a2467310d5a
Cristobal Sfeir <cnsfeir@uc.cl>
2023-07-22 18:23:11 -0400

    ✨ Authenticate by x-api-key header

--- middlewares/authentication.py
+++ middlewares/authentication.py
@@ -1,41 +1,25 @@
 # pylint: disable=raise-missing-from
 
-import os
-import re
 from http import HTTPStatus
 from typing import Annotated
 
-from dotenv import load_dotenv
 from fastapi import Header
 from fastapi.exceptions import HTTPException
 from fastapi.requests import Request
 
 from integrations.firestore import firestore_client
 
-load_dotenv()
 
-API_KEY = os.getenv("API_KEY")
-
-
-# TODO: Change the API Gateway configuration to receive the API key in the header and send it in the header
-async def authenticate(request: Request, x_envoy_original_path: Annotated[str | None, Header()] = None) -> None:
+async def authenticate(request: Request, x_api_key: Annotated[str | None, Header()] = None) -> None:
     """
     Authenticates a request.
     """
-    if not x_envoy_original_path:
-        raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED)
-
-    pattern = r"\?key=([^&]+)"
-    if not (match := re.search(pattern, x_envoy_original_path)):
+    if not x_api_key:
         raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED)
 
     try:
-        api_key = match.group(1)
-        user = firestore_client.get_user(api_key)
+        user = firestore_client.get_user(x_api_key)
     except (KeyError, ValueError):
         raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED)
-
-    user.register_call()
-    firestore_client.update_user(user)
 
     request.state.user = user
