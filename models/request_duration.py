3b4b87db012b73bf4607783f04e27b78fda19844
Cristobal Sfeir <cnsfeir@uc.cl>
2023-07-22 18:23:54 -0400

    🩹 Fix duration formatting

--- models/request_duration.py
+++ models/request_duration.py
@@ -1,16 +1,23 @@
+# pylint: disable=no-self-argument, no-member
+
 from enum import Enum
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 
 class DurationUnit(str, Enum):
-    MONTH = "month"
-    DAY = "day"
+    MONTH = "MONTH"
+    DAY = "DAY"
 
 
 class FundingRequestDuration(BaseModel):
-    unit: str = Field(..., alias="type")
+    unit: DurationUnit = Field(..., alias="type")
     value: int = Field(...)
 
     def __str__(self) -> str:
         return f"{self.value} {self.unit}"
+
+    @validator("unit", pre=True)
+    def unit_formatter(cls, value: str) -> DurationUnit:
+        """Formats the unit value"""
+        return DurationUnit(value.strip().upper())
