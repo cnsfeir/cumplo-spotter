a34da129eafe6b496de7b5b56522aa4a303913bd
Cristobal Sfeir <cnsfeir@uc.cl>
2023-07-22 18:18:02 -0400

    🩹 Set default factory for funding_requests attr

--- schemas/funding_requests.py
+++ schemas/funding_requests.py
@@ -5,4 +5,4 @@ from models.funding_request import FundingRequest
 
 class FilterFundingRequestPayload(BaseModel):
     id_user: str = Field("")
-    funding_requests: list[FundingRequest]
+    funding_requests: list[FundingRequest] = Field(default_factory=list)
