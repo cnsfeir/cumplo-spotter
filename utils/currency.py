a2edbe236504326fc0b9a7fac154efbcba1efe22
Cristobal Sfeir <cnsfeir@uc.cl>
2023-08-09 19:07:21 -0400

    🔥 Remove files moved to cumplo-common

--- utils/currency.py
+++
@@ -1,6 +0,0 @@
-import babel.numbers
-
-
-def format_currency(amount: int) -> str:
-    """Formats an amount of money in CLP"""
-    return babel.numbers.format_currency(amount, currency="CLP", locale="es_CL")
