71b6611a61ab13c46fcc89c00d9d6c9f303255d1
Cristobal Sfeir <cnsfeir@uc.cl>
2023-08-13 17:29:59 -0400

    ⚰️ Remove unused schemas

--- schemas/funding_requests.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-
-from models.funding_request import FundingRequest
-
-
-class FilterFundingRequestPayload(BaseModel):
-    id_user: str = Field("")
-    funding_requests: list[FundingRequest] = Field(default_factory=list)
