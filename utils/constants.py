08e754bd12c9752b32368f58c8c6c3bded969007
Cristobal Sfeir <cnsfeir@uc.cl>
2023-08-13 17:28:42 -0400

    ⚰️ Remove unused constant

--- utils/constants.py
+++ utils/constants.py
@@ -21,7 +21,6 @@ IRS_SECTOR_SELECTOR = "strong.loan-view-primary-color + span"
 IS_TESTING = bool(os.getenv("IS_TESTING"))
 LOCATION = os.getenv("LOCATION", "us-central1")
 LOG_FORMAT = "\n [%(levelname)s] %(message)s"
-MAX_CONFIGURATIONS = int(os.getenv("MAX_CONFIGURATIONS", "3"))
 NOTIFICATIONS_COLLECTION = os.getenv("NOTIFICATIONS_COLLECTION", "notifications")
 PAID_FUNDING_REQUESTS_COUNT_SELECTOR = "div.loan-view-item span:nth-of-type(1)"
 PAID_IN_TIME_PERCENTAGE_SELECTOR = "div.loan-view-item span:nth-of-type(5)"
