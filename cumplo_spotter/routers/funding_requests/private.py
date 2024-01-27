cc4fdfff5fbd9b3ad3cf034fd1e842e90339abf3
Cristobal Sfeir <cnsfeir@uc.cl>
2024-01-27 14:10:42 -0300

    ✨ Add PubSub communication

--- cumplo_spotter/routers/funding_requests/private.py
+++ cumplo_spotter/routers/funding_requests/private.py
@@ -1,54 +1,39 @@
 # pylint: disable=no-member
 
-import json
 from http import HTTPStatus
 from logging import getLogger
 from typing import cast
 
-from cumplo_common.database import firestore
-from cumplo_common.integrations.cloud_tasks import create_http_task
+from cumplo_common.integrations.cloud_pubsub import publish_event
 from cumplo_common.models.funding_request import FundingRequest
-from cumplo_common.models.template import Template
-from cumplo_common.models.topic import Topic
 from cumplo_common.models.user import User
 from fastapi import APIRouter
 from fastapi.requests import Request
 
 from cumplo_spotter.business import funding_requests
-from cumplo_spotter.integrations import cumplo
-from cumplo_spotter.utils.constants import (
-    CUMPLO_HERALD_QUEUE,
-    CUMPLO_HERALD_URL,
-    CUMPLO_SPOTTER_QUEUE,
-    CUMPLO_SPOTTER_URL,
-)
+from cumplo_spotter.utils.constants import AVAILABLE_FUNDING_REQUESTS_TOPIC, PROMISING_FUNDING_REQUESTS_TOPIC
 
 logger = getLogger(__name__)
-
 
 router = APIRouter(prefix="/funding-requests")
 
 
 @router.post(path="/fetch", status_code=HTTPStatus.NO_CONTENT)
-async def _fetch_funding_requests(_request: Request) -> None:
+def _fetch_funding_requests(request: Request) -> None:
     """
-    Fetches a list of funding requests and schedules the filtering process.
+    Fetches a list of funding requests and emits an event containing them.
     """
-    available_funding_requests = await cumplo.get_available_funding_requests()
-    body = {funding_request.id: funding_request.export() for funding_request in available_funding_requests}
+    user = cast(User, request.state.user)
 
-    for user in firestore.client.users.get_all():
-        create_http_task(
-            url=f"{CUMPLO_SPOTTER_URL}/funding-requests/filter",
-            task_id=f"filter-funding-requests-{user.id}",
-            headers={"x-api-key": user.api_key},
-            queue=CUMPLO_SPOTTER_QUEUE,
-            payload=body,
-        )
+    available_funding_requests = funding_requests.get_available()
+    logger.info(f"Found {len(available_funding_requests)} available funding requests")
+
+    content = [funding_request.json() for funding_request in available_funding_requests]
+    publish_event(content, AVAILABLE_FUNDING_REQUESTS_TOPIC, id_user=str(user.id))
 
 
 @router.post(path="/filter", status_code=HTTPStatus.NO_CONTENT)
-async def _filter_funding_requests(request: Request, payload: dict[str, FundingRequest]) -> None:
+def _filter_funding_requests(request: Request, payload: list[FundingRequest]) -> None:
     """
     Filters a list of funding requests based on the user's filters.
     """
@@ -56,7 +41,7 @@ async def _filter_funding_requests(request: Request, payload: dict[str, FundingR
 
     promising_funding_requests = set()
     for filters in user.filters.values():
-        promising_funding_requests.update(funding_requests.filter_(list(payload.values()), filters))
+        promising_funding_requests.update(funding_requests.filter_(list(payload), filters))
 
     if not promising_funding_requests:
         logger.info(f"No promising funding requests for user {user.id}")
@@ -66,10 +51,4 @@ async def _filter_funding_requests(request: Request, payload: dict[str, FundingR
 
     for funding_request in promising_funding_requests:
         logger.info(f"Notifying about funding requests {funding_request.id} to user {user.id}")
-        create_http_task(
-            url=f"{CUMPLO_HERALD_URL}/{Topic.FUNDING_REQUESTS}/{Template.PROMISING}/notify",
-            task_id=f"send-funding-requests-{funding_request.id}-webhook-{user.id}",
-            headers={"x-api-key": user.api_key},
-            queue=CUMPLO_HERALD_QUEUE,
-            payload=json.loads(funding_request.model_dump_json()),
-        )
+        publish_event(funding_request.json(), PROMISING_FUNDING_REQUESTS_TOPIC, id_user=str(user.id))
--- cumplo_spotter/routers/funding_requests/public.py
+++ cumplo_spotter/routers/funding_requests/public.py
@@ -17,15 +17,19 @@ router = APIRouter(prefix="/funding-requests")
 
 
 @router.get("", status_code=HTTPStatus.OK)
-async def _get_funding_requests(_request: Request) -> list[dict]:
-    """Gets a list of available funding requests"""
-    available_funding_requests = await funding_requests.get_available()
-    return [request.model_dump() for request in available_funding_requests]
+def _get_funding_requests(_request: Request) -> list[dict]:
+    """
+    Gets a list of available funding requests
+    """
+    available_funding_requests = funding_requests.get_available()
+    return [funding_request.json() for funding_request in available_funding_requests]
 
 
 @router.get("/promising", status_code=HTTPStatus.OK)
-async def _get_promising_funding_requests(request: Request) -> list[dict]:
-    """Gets a list of promising funding requests based on the user's configuration"""
+def _get_promising_funding_requests(request: Request) -> list[dict]:
+    """
+    Gets a list of promising funding requests based on the user's configuration
+    """
     user = cast(User, request.state.user)
-    promising_funding_requests = await funding_requests.get_promising(user)
-    return [request.model_dump() for request in promising_funding_requests]
+    promising_funding_requests = funding_requests.get_promising(user)
+    return [request.json() for request in promising_funding_requests]
